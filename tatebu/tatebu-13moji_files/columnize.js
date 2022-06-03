870a161dad49e298a52609e3f2deecf972d4fcba
take-i <take@gpl.jp>
2022-06-03 15:21:21 +0900

    PWA対応

--- tatebu/tatebu-13moji_files/columnize.js
+++
@@ -1,7 +0,0 @@
-    //jQuery.noConflict();
-    jQuery(document).ready(function($){
-    
-        jQuery('.mcol').makeacolumnlists({cols: 3, colWidth: 305, equalHeight: 'ul', startN: 1});
-        jQuery('.mcol2').makeacolumnlists({cols: 3, colWidth: 293, equalHeight: 'ul', startN: 1});
-       
-    });
