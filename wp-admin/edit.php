1dec6af82e4051ad9fb4907dfec4b360a2466a55
Ishizaka Takehiko <ishizaka@gpl.jp>
2023-07-31 02:27:48 +0900

    投稿日: 2023年7月30日 ベトナムでTNT125が激安・レビュー

---
+++ wp-admin/edit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>リダイレクト中...</title>
+		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D209491&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D209491&reauth=1";
+		</script>
+
+		<p><a href="/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D209491&reauth=1">/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D209491&reauth=1</a> にリダイレクト中</p>
+	</body>
+</html>
