cbbf46656ef181b79ac08e48ece0cccef788c2e4
JunkHack <junkhack@gpl.jp>
2024-02-25 05:00:16 +0900

    ブログ更新

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>リダイレクト中...</title>
-		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D331601&reauth=1">
+		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D344282&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D331601&reauth=1";
+			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D344282&reauth=1";
 		</script>
 
-		<p><a href="/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D331601&reauth=1">/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D331601&reauth=1</a> にリダイレクト中</p>
+		<p><a href="/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D344282&reauth=1">/wp-login.php?redirect_to=https%3A%2F%2Fhack.gpl.jp%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D344282&reauth=1</a> にリダイレクト中</p>
 	</body>
 </html>
